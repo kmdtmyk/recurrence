b4afa9b27e601b11368b362c00b8a40f7134f872
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-06-22 09:01:28 +0000

    add daily test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 
 export default class{
 
+  constructor(options={}){
+    Object.assign(this, options)
+  }
+
+  match(date){
+    return this.start <= date
+  }
 
 }
--- test/recurrence.test.js
+++ test/recurrence.test.js
@@ -1,11 +1,20 @@
 import assert from 'assert'
 import Recurrence from '../src/index.js'
 
+describe('Recurrence', () => {
 
-describe('test', function(){
+  describe('match', () => {
 
-  it('foo', function(){
-    assert.equal(1 + 2, 3)
+    it('daily', () => {
+      let r = new Recurrence({
+        start: '2016/06/22',
+      })
+
+      assert.equal(r.match('2016/06/21'), false)
+      assert.equal(r.match('2016/06/22'), true)
+      assert.equal(r.match('2016/06/23'), true)
+    })
+
   })
 
 })
