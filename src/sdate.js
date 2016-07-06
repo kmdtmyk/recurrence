27b92ad7b1add6735b907d8a1356679a6f6116be
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-07-06 01:42:14 +0000

    remove magic number

--- src/sdate.js
+++ src/sdate.js
@@ -1,12 +1,16 @@
 
+const SECOND = 1000
+const MINUTE = SECOND * 60
+const HOUR = MINUTE * 60
+const DAY = HOUR * 24
+
 
 export default class{
 
   static diffInDay(date1, date2){
     let d1 = new Date(date1)
     let d2 = new Date(date2)
-    let diffInMilliSec = d2 - d1
-    return (d2 - d1) / ( 1000 * 60 * 60 * 24 )
+    return (d2 - d1) / DAY
   }
 
   static equal(){
