f339b2b2cdb617de8417e6c439ff90ba4b8225a8
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-07-06 01:33:37 +0000

    add daily interval

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
+
+class Sdate{
+
+  static diffInDay(date1, date2){
+    let d1 = new Date(date1)
+    let d2 = new Date(date2)
+    let diffInMilliSec = d2 - d1
+    return (d2 - d1) / ( 1000 * 60 * 60 * 24 )
+  }
+
+  static equal(){
+
+  }
+
+}
+
 export default class{
 
   constructor(options={}){
-    Object.assign(this, options)
+    this.startDate = options.startDate
+    this.interval = options.interval || 1
   }
 
-  match(date){
-    return this.start <= date
+  include(date){
+    let diffInDay = Sdate.diffInDay(this.startDate, date)
+    return this.startDate <= date && ( diffInDay % this.interval ) === 0
   }
 
 }
--- test/recurrence.test.js
+++ test/recurrence.test.js
@@ -3,16 +3,27 @@ import Recurrence from '../src/index.js'
 
 describe('Recurrence', () => {
 
-  describe('match', () => {
+  describe('include', () => {
 
     it('daily', () => {
       let r = new Recurrence({
-        start: '2016/06/22',
+        startDate: '2016/06/22',
       })
 
-      assert.equal(r.match('2016/06/21'), false)
-      assert.equal(r.match('2016/06/22'), true)
-      assert.equal(r.match('2016/06/23'), true)
+      assert.equal(r.include('2016/06/21'), false)
+      assert.equal(r.include('2016/06/22'), true)
+      assert.equal(r.include('2016/06/23'), true)
+    })
+
+    it('daily　interval', () => {
+      let r = new Recurrence({
+        startDate: '2016/06/22',
+        interval: 2,
+      })
+
+      assert.equal(r.include('2016/06/22'), true)
+      assert.equal(r.include('2016/06/23'), false)
+      assert.equal(r.include('2016/06/24'), true)
     })
 
   })
