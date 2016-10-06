dea24b4e0b4f2ded53b5108ad9f8f11720dfdfa9
kmdtmyk <winback_banderas@yahoo.co.jp>
2016-10-06 19:01:37 +0900

    [Calendar] diffInWeek

--- src/calendar.js
+++ src/calendar.js
@@ -4,6 +4,23 @@ import DayOfWeek from './DayOfWeek'
 
 export default class extends DayOfWeek {
 
+  static diffInWeek(date1, date2, dayOfWeek){
+    if(Sdate.isValid(date1) === false || Sdate.isValid(date1) === false){
+      return NaN
+    }
+    if(DayOfWeek.isValidDayOfWeek(dayOfWeek)){
+      date2 = this.startOfWeek(date2, dayOfWeek)
+      return Math.floor(Sdate.diffInDay(date1, date2) / 7) + 1
+    }
+    if(dayOfWeek !== null && dayOfWeek !== undefined){
+      return NaN
+    }
+    if(Sdate.lessThanOrEqual(date1, date2)){
+      return Math.floor(Sdate.diffInDay(date1, date2) / 7)
+    }
+    return - Math.floor(Sdate.diffInDay(date2, date1) / 7)
+  }
+
   static startOfMonth(date, dayOfWeek){
     if(Sdate.isValid(date) === false || DayOfWeek.isValidDayOfWeek(dayOfWeek) === false){
       return ''
